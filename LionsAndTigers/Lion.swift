370a8836aea00a1b266f2a10247dc95152730912
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-07-09 22:09:42 -0500

    testing merging from experimental into master

--- LionsAndTigers/Lion.swift
+++ LionsAndTigers/Lion.swift
@@ -15,6 +15,7 @@ class Lion {
     var image = UIImage(named:"")
     var name = ""
     var subspecies = ""
+    //testing merge from experimental branch into master
     
     func roar() {
         println("Lion: Roar Roar")
